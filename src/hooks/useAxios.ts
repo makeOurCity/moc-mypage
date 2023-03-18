c1e7b0a881b7bd38de05055fb0b5dea79919ce10
Shugo USHIO <ushio.s@gmail.com>
2023-03-19 00:00:09 +0900

    Use NEXT_PUBLIC_

--- src/hooks/useAxios.ts
+++ src/hooks/useAxios.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 import { makeUseAxios } from "axios-hooks";
 
 const instance = axios.create({
-  baseURL: process.env.ORION_BASE_URL,
-  timeout: Number.parseInt(process.env.ORION_TIMEOUT || "10000")
+  baseURL: process.env.NEXT_PUBLIC_ORION_BASE_URL,
+  timeout: Number.parseInt(process.env.NEXT_PUBLIC_ORION_TIMEOUT || "10000")
 });
 
 export const useAxios = makeUseAxios({
--- src/pages/fiware/orion/types/index.tsx
+++ src/pages/fiware/orion/types/index.tsx
@@ -2,7 +2,10 @@ import { Layout } from "@/components/layout";
 import { useAxios } from "@/hooks/useAxios";
 
 export default function FiwareOrionTypesIndex() {
-  const { axios } = useAxios();
+  const [{ data, loading, error }, refetch] = useAxios('/v2/types');
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error!</p>
 
   return <Layout><>test</></Layout>;
 }
