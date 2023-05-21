7ffae2917dc98b66c2de7e74701c481f85eaa26d
Shugo USHIO <ushio.s@gmail.com>
2023-05-21 18:13:53 +0900

    change name

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import LoginButton from "@/components/LoginButton";
-import { Layout } from "@/components/layouta";
+import { Layout } from "@/components/Layout";
 import { useSession } from "next-auth/react";
 
 const inter = Inter({ subsets: ["latin"] });
