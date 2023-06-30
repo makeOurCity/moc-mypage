5b39f51d859e9762912a10a0ab23f5c3b6d9c103
Shugo USHIO <ushio.s@gmail.com>
2023-06-30 14:25:22 +0900

    redirect when sign out

--- src/components/header/Menu.tsx
+++ src/components/header/Menu.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback } from "react";
 import {
   useColorModeValue,
   MenuDivider,
@@ -6,10 +6,18 @@ import {
   MenuList,
 } from "@chakra-ui/react";
 import {  signOut } from "next-auth/react";
+import { useRouter } from "next/router";
 
 export default function HeaderMenu() {
+  const router = useRouter();
   const white = useColorModeValue("white", "gray.900");
   const gray = useColorModeValue("gray.200", "gray.700");
+
+  const handleSignOut = useCallback(async () => {
+    const respo = await signOut({
+      callbackUrl: "/",
+    });
+  }, []);
 
   return (
     <MenuList bg={white} borderColor={gray}>
@@ -18,7 +26,7 @@ export default function HeaderMenu() {
       <MenuItem>Billing</MenuItem>
       <MenuDivider />
 
-      <MenuItem onClick={() => signOut()}>Sign Out</MenuItem>
+      <MenuItem onClick={() => handleSignOut()}>Sign Out</MenuItem>
     </MenuList>
   );
 }
