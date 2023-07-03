edffaf5d0b5d804ec5a69c8fed8a040410e870de
Shugo USHIO <ushio.s@gmail.com>
2023-07-04 00:46:37 +0900

    Add button

--- src/components/orion/SubscriptionList.tsx
+++ src/components/orion/SubscriptionList.tsx
@@ -1,21 +1,37 @@
-import { Center, Table, TableContainer, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
-import { ListEntitiesResponse, ListEntityTypesResponse, ListSubscriptionsResponse } from "../../../codegens/orion";
+import {
+  Center,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+import {
+  ListEntitiesResponse,
+  ListEntityTypesResponse,
+  ListSubscriptionsResponse,
+} from "../../../codegens/orion";
+import SubscriptionDeleteButton from "./SubscriptionDeleteButton";
 
 type Props = {
   data: ListSubscriptionsResponse[];
 };
 
 export default function SubscriptionList({ data }: Props) {
+  const deleteButton = SubscriptionDeleteButton();
 
   const list = data.map((t) => {
     return (
       <Tr key={t.id}>
-        <Td>{ t.id }</Td>
-        <Td>{ t.status }</Td>
-        <Td>{ t.description }</Td>
-        <Td>{ t.expires || "なし" }</Td>
+        <Td>{t.id}</Td>
+        <Td>{t.status}</Td>
+        <Td>{t.description}</Td>
+        <Td>{t.expires || "なし"}</Td>
+        <Td>{deleteButton}</Td>
       </Tr>
-    )
+    );
   });
 
   return (
@@ -28,11 +44,10 @@ export default function SubscriptionList({ data }: Props) {
               <Th>ステータス</Th>
               <Th>説明</Th>
               <Th>期限</Th>
+              <Th>アクション</Th>
             </Tr>
           </Thead>
-          <Tbody>
-            { list }
-          </Tbody>
+          <Tbody>{list}</Tbody>
         </Table>
       </TableContainer>
     </Center>
