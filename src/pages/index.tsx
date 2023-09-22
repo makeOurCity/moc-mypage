6669b4b50ef9ed1c1caa0db6e628ec617d256bf2
Shugo USHIO <ushio.s@gmail.com>
2023-09-23 00:57:00 +0900

    show info

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,16 +6,28 @@ import LoginButton from "@/components/LoginButton";
 import { Layout } from "@/components/Layout";
 import { useSession } from "next-auth/react";
 import DataTableCard from "@/components/card/DataTable";
+import { useMocApi } from "@/hooks/useMocApi";
+import { useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const { data: session, status: loading } = useSession();
+  const { mocApi } = useMocApi();
 
-  const data = {"hoge": "hoga"};
+  const [basicData, setBasicData] = useState<{[key:string]: string}>({});
+
+  useEffect(() => {
+    // if (!session) return;
+
+    mocApi.get('/api/info').then((res) => {
+      setBasicData(res.data);
+    })
+  }, []);
+
   return (
     <Layout>
-      <DataTableCard title="接続情報" data={data} />
+      <DataTableCard title="接続情報" data={basicData} />
     </Layout>
   );
 }
