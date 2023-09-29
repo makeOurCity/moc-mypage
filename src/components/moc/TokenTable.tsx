635033ea1f8971ec85f71e70c82fd2e750e96adc
Shugo USHIO <ushio.s@gmail.com>
2023-09-29 18:01:33 +0900

    Add component

--- src/components/moc/TokenTable.tsx
+++ src/components/moc/TokenTable.tsx
@@ -5,36 +5,36 @@ import {
   Heading,
   Button,
   CardFooter,
+  useBoolean,
 } from "@chakra-ui/react";
 import useAxios from "axios-hooks";
 import { title } from "process";
+import { ReactNode, useEffect, useState } from "react";
 
 export default function MocTokenTable() {
-  const [
-    { data: token, loading: loadingToken, error: getTokenError },
-    getToken,
-  ] = useAxios({ url: "/api/token", method: "GET" }, { manual: true });
-
-  function loadToken() {
-    getToken();
-  }
 
   const title = "トークン情報";
-  const body = [];
-  body.push(
-    "表示するボタンをクリックすると、トークン情報がここに表示されます。第三者に情報を見られないように注意してください。"
-  );
+  const defaultBody = "表示するボタンをクリックすると、トークン情報がここに表示されます。第三者に情報を見られないように注意してください。"
+
+  const [buttonLabel, setButtonLabel] = useState<string>("表示する");
+  const [body, setBody] = useState<string>(defaultBody);
+  const [viewFlag, setViewFlag] = useBoolean(false);
+
+  // const [
+  //   { data: token, loading: loadingToken, error: getTokenError },
+  //   getToken,
+  // ] = useAxios({ url: "/api/token", method: "GET" }, { manual: true });
 
   return (
     <Card>
       <CardHeader>
         <Heading as="h1" size="md">
-          {title}
+          { title }
         </Heading>
       </CardHeader>
-      <CardBody>{body}</CardBody>
+      <CardBody>{ body }</CardBody>
       <CardFooter>
-        <Button onClick={loadToken}>表示する</Button>
+        <Button>{ buttonLabel }</Button>
       </CardFooter>
     </Card>
   );
