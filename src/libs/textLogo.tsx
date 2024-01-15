5ba234b8bb0d553d758804fe7924a7b099c49b15
Shugo USHIO <ushio.s@gmail.com>
2024-01-15 11:19:36 +0900

    update path

--- src/libs/textLogo.tsx
+++ src/libs/textLogo.tsx
@@ -1,8 +1,10 @@
 import fs from "fs";
 import satori from "satori";
+import path from "path";
 
 export default async function TextLogo(text: string) {
-  const fontData = fs.readFileSync("public/fonts/NotoSerifJP-Bold.otf");
+  const fontPath = path.join(process.cwd(), "public", "fonts", "NotoSerifJP-Bold.otf");
+  const fontData = fs.readFileSync(fontPath);
   // const fontData = await fetch("https://fonts.googleapis.com/css2?family=Noto+Serif+JP&display=swap").then((resp) => resp.arrayBuffer());
 
   const svg = await satori(
