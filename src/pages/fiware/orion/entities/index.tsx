243fc428a5c5b04dc8bc15d4351aa7ff5bcde08f
yu23ki14 <yuki_021423@yahoo.co.jp>
2024-12-25 08:45:13 +0900

    add property search

--- src/pages/fiware/orion/entities/index.tsx
+++ src/pages/fiware/orion/entities/index.tsx
@@ -6,50 +6,89 @@ import {
   Button,
   Heading,
   Stack,
-  Icon,
   Card,
   CardHeader,
   CardBody,
   Text,
   Flex,
-  Box,
   Input,
+  HStack,
+  VStack,
 } from "@chakra-ui/react";
 import { useCallback, useEffect, useState } from "react";
-import { FiRefreshCw } from "react-icons/fi";
 import { ListEntitiesResponse } from "@/codegens/orion";
 import { useLocalStorage } from "@/hooks/useLocalStorage";
 import Link from "next/link";
+import { Controller, useFieldArray, useForm } from "react-hook-form";
+
+interface FormProps {
+  type: string;
+  attributes: {
+    key: string;
+    value: string;
+  }[];
+}
 
 export default function FiwareOrionEntitiesIndex() {
   const { api, setFiwareServiceHeader } = useOrion();
   const [list, setList] = useState<ListEntitiesResponse[]>([]);
   const [fiwareService, setFiwareService, loadingLocalStorage] =
     useLocalStorage<string | undefined>("fiware-service", undefined);
-  const [type, setType] = useState<string | undefined>(undefined);
+
+  const { control, handleSubmit } = useForm<FormProps>();
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "attributes",
+  });
 
   /**
    * 更新ボタンクリック時の挙動
    */
-  const updateList = useCallback(() => {
-    logger.info("Update entity list ", fiwareService);
+  const updateList = useCallback(
+    (data: FormProps) => {
+      logger.info("Update entity list ", fiwareService);
 
-    if (fiwareService) {
-      setFiwareServiceHeader(fiwareService);
-    }
+      if (fiwareService) {
+        setFiwareServiceHeader(fiwareService);
+      }
 
-    const _ = undefined;
+      const _ = undefined;
 
-    api.entitiesApi
-      .listEntities(_, type ? type : _, _, _, _, _, _, _, _, 100, _, _)
-      .then((res) => {
-        setList(res.data);
-      });
-  }, [loadingLocalStorage, type]);
+      const type = data.type;
+
+      const attributes = data.attributes
+        .filter((attr) => attr.key && attr.value)
+        .map((attr) => `${attr.key}~=${attr.value}`)
+        .join(";");
+
+      api.entitiesApi
+        .listEntities(
+          _,
+          type ? type : _,
+          _,
+          _,
+          attributes ? attributes : _,
+          _,
+          _,
+          _,
+          _,
+          100,
+          _,
+          _
+        )
+        .then((res) => {
+          setList(res.data);
+        });
+    },
+    [loadingLocalStorage]
+  );
 
   useEffect(() => {
     if (loadingLocalStorage === false) {
-      updateList();
+      updateList({
+        type: "",
+        attributes: [],
+      });
     }
   }, [loadingLocalStorage]);
 
@@ -62,19 +101,61 @@ export default function FiwareOrionEntitiesIndex() {
               <Stack rowGap={2}>
                 <Heading as="h1" size="md">
                   エンティティ一覧 &nbsp;
-                  <Button variant="ghost" onClick={updateList}>
-                    <Icon as={FiRefreshCw} />
-                  </Button>
                 </Heading>
                 <Text>Fiware-Service: {fiwareService}</Text>
-                <Flex gap={2}>
-                  Type:
-                  <Input
-                    value={type}
-                    onChange={(e) => setType(e.target.value)}
-                    size="sm"
-                  />
-                </Flex>
+
+                <form onSubmit={handleSubmit(updateList)}>
+                  <VStack gap={2} align="stretch">
+                    <HStack gap={2}>
+                      <Text>Type:</Text>
+                      <Controller
+                        control={control}
+                        name="type"
+                        render={({ field }) => <Input {...field} size="sm" />}
+                      />
+                    </HStack>
+                    {fields.map((field, index) => (
+                      <HStack key={field.id} gap={2}>
+                        <Controller
+                          control={control}
+                          name={`attributes.${index}.key`}
+                          render={({ field }) => (
+                            <Input
+                              {...field}
+                              size="sm"
+                              placeholder="プロパティ名"
+                            />
+                          )}
+                        />
+                        <Controller
+                          control={control}
+                          name={`attributes.${index}.value`}
+                          render={({ field }) => (
+                            <Input {...field} size="sm" placeholder="値" />
+                          )}
+                        />
+                        <Button
+                          size="sm"
+                          colorScheme="red"
+                          onClick={() => remove(index)}
+                        >
+                          削除
+                        </Button>
+                      </HStack>
+                    ))}
+                    <Button
+                      w="full"
+                      colorScheme="gray"
+                      size="sm"
+                      onClick={() => append({ key: "", value: "" })}
+                    >
+                      条件を追加
+                    </Button>
+                    <Button w="full" colorScheme="blue" type="submit" size="sm">
+                      検索
+                    </Button>
+                  </VStack>
+                </form>
               </Stack>
               <Flex gap={2}>
                 <Link href="/fiware/orion/entities/csv">
