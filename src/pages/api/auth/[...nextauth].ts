376ddc45834cf3cac2471aac27ec9d65f2e59020
Shugo USHIO <ushio.s@gmail.com>
2025-02-09 11:06:33 +0900

    add provider

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,221 +1,44 @@
-import NextAuth from "next-auth";
-import Auth0Provider from "next-auth/providers/auth0";
-import { OAuthConfig } from "next-auth/providers";
-import FacebookProvider from "next-auth/providers/facebook";
-import GithubProvider from "next-auth/providers/github";
-import GoogleProvider from "next-auth/providers/google";
-import TwitterProvider from "next-auth/providers/twitter";
-import CognitoProvider from "next-auth/providers/cognito";
-import { Provider } from "next-auth/providers";
+import NextAuth, { type NextAuthOptions } from "next-auth";
 
-import axios from "axios";
-import { getToken } from "next-auth/jwt";
-// import EmailProvider from "next-auth/providers/email"
-// import AppleProvider from "next-auth/providers/apple"
-
-const providers: Provider[] = [];
-
-if (process.env.KONG_CLIENT_ID && process.env.KONG_CLIENT_SECRET) {
-  providers.push({
-    id: "kong",
-    name: "Kong",
-    type: "oauth",
-    // wellKnown: `${process.env.KONG_ISSUER}/.well-known/openid-configuration`,
-    token: {
-      url: `${process.env.KONG_URL}/oauth2/token`,
-      request: async ({ client, provider, params, checks }) => {
-        const tokens = await client.grant({
-          grant_type: "authorization_code",
-          code: params.code,
-          redirect_uri: provider.callbackUrl,
-          code_verifier: checks.code_verifier,
-        });
-        return { tokens };
-      },
-    },
-    // userinfo: `${process.env.KONG_URL}/api/v4/user`,
-    authorization: {
-      url: `${process.env.KONG_URL}/oauth2/authorize`,
-      params: { scope: "openid email profile" },
-      request: async ({ provider, client }) => {
-        const url = await client.authorizationUrl({
-          scope: provider.authorization?.params?.scope,
-          redirect_uri: provider.callbackUrl,
-          response_type: "code",
-          method: "POST",
-        });
-        return { url };
-      },
-    },
-    idToken: true,
-    clientId: process.env.KONG_CLIENT_ID,
-    clientSecret: process.env.KONG_CLIENT_SECRET,
-    profile(profile: any) {
-      return {
-        id: profile.sub,
-        name: profile.name,
-        email: profile.email,
-        image: profile.picture,
-      };
-    },
-  } as OAuthConfig<any>);
-}
-
-// https://mseeeen.msen.jp/nextauth-cognito-token-refresh/
-// https://kelvinmwinuka.com/social-login-with-cognito-and-nextauth
-if (process.env.COGNITO_CLIENT_ID && process.env.COGNITO_CLIENT_SECRET) {
-  providers.push(
-    CognitoProvider({
-      clientId: process.env.COGNITO_CLIENT_ID!,
-      clientSecret: process.env.COGNITO_CLIENT_SECRET!,
-      issuer: process.env.COGNITO_ISSUER,
-      idToken: true,
+export const authOptions: NextAuthOptions = {
+  debug: true,
+  providers: [
+    {
+      id: "kong", // プロバイダーのID
+      name: "Kong", // プロバイダーの名前
+      type: "oauth", // プロバイダーのタイプ
+      clientId: process.env.KONG_CLIENT_ID, // クライアントID
+      clientSecret: process.env.KONG_CLIENT_SECRET, // クライアントシークレット
       authorization: {
-        params: {
-          scope: "openid email profile",
-        },
+        url: `${process.env.KONG_URL}/oauth2/authorize`, // 認証エンドポイント
+        params: { scope: "email profile" }, // スコープ
       },
-    })
-  );
-}
-
-// For more information on each option (and a full list of options) go to
-// https://next-auth.js.org/configuration/options
-export default NextAuth({
-  // https://next-auth.js.org/configuration/providers
-  providers,
-  // providers: [
-  // CognitoProvider({
-  //   clientId: process.env.COGNITO_CLIENT_ID!,
-  //   clientSecret: process.env.COGNITO_CLIENT_SECRET!,
-  //   issuer: process.env.COGNITO_ISSUER,
-  // })
-  // EmailProvider({
-  //   server: process.env.EMAIL_SERVER,
-  //   from: process.env.EMAIL_FROM,
-  // }),
-  // AppleProvider({
-  //   clientId: process.env.APPLE_ID,
-  //   clientSecret: {
-  //     appleId: process.env.APPLE_ID,
-  //     teamId: process.env.APPLE_TEAM_ID,
-  //     privateKey: process.env.APPLE_PRIVATE_KEY,
-  //     keyId: process.env.APPLE_KEY_ID,
-  //   },
-  // }),
-  // Auth0Provider({
-  //   clientId: process.env.AUTH0_ID,
-  //   clientSecret: process.env.AUTH0_SECRET,
-  //   // @ts-ignore
-  //   domain: process.env.AUTH0_DOMAIN,
-  // }),
-  // FacebookProvider({
-  //   clientId: process.env.FACEBOOK_ID,
-  //   clientSecret: process.env.FACEBOOK_SECRET,
-  // }),
-  // GithubProvider({
-  //   clientId: process.env.GITHUB_ID,
-  //   clientSecret: process.env.GITHUB_SECRET,
-  //   // https://docs.github.com/en/developers/apps/building-oauth-apps/scopes-for-oauth-apps
-  //   // @ts-ignore
-  //   scope: "read:user",
-  // }),
-  // GoogleProvider({
-  //   clientId: process.env.GOOGLE_ID,
-  //   clientSecret: process.env.GOOGLE_SECRET,
-  // }),
-  // TwitterProvider({
-  //   clientId: process.env.TWITTER_ID,
-  //   clientSecret: process.env.TWITTER_SECRET,
-  // }),
-  // ],
-  // Database optional. MySQL, Maria DB, Postgres and MongoDB are supported.
-  // https://next-auth.js.org/configuration/databases
-  //
-  // Notes:
-  // * You must install an appropriate node_module for your database
-  // * The Email provider requires a database (OAuth providers do not)
-  // database: process.env.DATABASE_URL,
-
-  // The secret should be set to a reasonably long random string.
-  // It is used to sign cookies and to sign and encrypt JSON Web Tokens, unless
-  // a separate secret is defined explicitly for encrypting the JWT.
-  secret: process.env.SECRET,
-
-  session: {
-    // Use JSON Web Tokens for session instead of database sessions.
-    // This option can be used with or without a database for users/accounts.
-    // Note: `strategy` should be set to 'jwt' if no database is used.
-    strategy: "jwt",
-
-    // Seconds - How long until an idle session expires and is no longer valid.
-    // maxAge: 30 * 24 * 60 * 60, // 30 days
-
-    // Seconds - Throttle how frequently to write to database to extend a session.
-    // Use it to limit write operations. Set to 0 to always update the database.
-    // Note: This option is ignored if using JSON Web Tokens
-    // updateAge: 24 * 60 * 60, // 24 hours
-  },
-
-  // JSON Web tokens are only used for sessions if the `strategy: 'jwt'` session
-  // option is set - or by default if no database is specified.
-  // https://next-auth.js.org/configuration/options#jwt
-  jwt: {
-    // A secret to use for key generation (you should set this explicitly)
-    secret: process.env.SECRET,
-    // Set to true to use encryption (default: false)
-    // encryption: true,
-    // You can define your own encode/decode functions for signing and encryption
-    // if you want to override the default behaviour.
-    // encode: async ({ secret, token, maxAge }) => {},
-    // decode: async ({ secret, token, maxAge }) => {},
-  },
-
-  // You can define custom pages to override the built-in ones. These will be regular Next.js pages
-  // so ensure that they are placed outside of the '/api' folder, e.g. signIn: '/auth/mycustom-signin'
-  // The routes shown here are the default URLs that will be used when a custom
-  // pages is not specified for that route.
-  // https://next-auth.js.org/configuration/pages
-  pages: {
-    // signIn: '/auth/signin',  // Displays signin buttons
-    // signOut: '/auth/signout', // Displays form with sign out button
-    // error: '/auth/error', // Error code passed in query string as ?error=
-    // verifyRequest: '/auth/verify-request', // Used for check email page
-    // newUser: null // If set, new users will be directed here on first sign in
-  },
-
-  // Callbacks are asynchronous functions you can use to control what happens
-  // when an action is performed.
-  // https://next-auth.js.org/configuration/callbacks
-  callbacks: {
-    async signIn({ user, account, profile, email, credentials }) {
-      return true;
-    },
-    // async redirect({ url, baseUrl }) { return baseUrl },
-    async session({ session, token, user }) {
-      // session.accessToken = token.accessToken;
-      session.idToken = token.idToken;
-      // session.refreshToken = token.refreshToken;
-      return session;
-    },
-    // async jwt({ token, user, account, profile, isNewUser }) { return token }
-    async jwt({ token, user, account, profile, isNewUser }) {
-      if (account) {
-        // account is provided upon the inital auth
+      token: `${process.env.KONG_URL}/oauth2/token`, // トークンエンドポイント
+      userinfo: `${process.env.KONG_URL}/oauth2/userinfo`, // ユーザー情報エンドポイント
+      profile: (profile: any) => {
         return {
-          ...token,
-          // accessToken: account.access_token,
-          idToken: account.id_token,
-          // refreshToken: account.refresh_token,
+          id: profile.sub, // ユーザーID
+          name: profile.name, // ユーザー名
+          email: profile.email, // メールアドレス
         };
+      },
+    },
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
       }
       return token;
     },
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id as string;
+      }
+      return session;
+    },
   },
-  // Events are useful for logging
-  // https://next-auth.js.org/configuration/events
-  events: {},
+  secret: process.env.NEXTAUTH_SECRET, // NextAuthのシークレットキー
+};
 
-  // Enable debug messages in the console if you are having problems
-  debug: process.env.NODE_ENV === "development" ? true : false,
-});
+export default NextAuth(authOptions);
